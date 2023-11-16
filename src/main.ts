cc60c1f952f2fd8e12c8403bb2ce3caa7d7e97b5
pljjy <jotaro5jostar@gmail.com>
2023-11-16 17:27:42 -0500

    did all the visuals

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import $ from 'jquery';
 import './style.css';
 
+console.clear()
 
+$("textarea").each(function() {
+  this.setAttribute("style", `height:${this.scrollHeight}px;overflow-y:hidden;`);
+}).on("input", function() {
+  this.style.height = "0";
+  this.style.height = (this.scrollHeight) + "px";
+});
+
+$('.del-sect').on('mouseenter', function() {
+  $('.edit').removeClass('hide');
+}).on('mouseleave', function() {
+  $('.edit').addClass('hide');
+});
