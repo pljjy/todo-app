d3127bb8e26e40fab480974850965c33a8626ccf
pljjy <jotaro5jostar@gmail.com>
2023-11-21 17:11:45 -0500

    moved .add listener to other listeners

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import './style.css';
 import $ from 'jquery';
 import domManager from './domManager';
+import { functions } from 'lodash';
 
 
 // default value because otherwise it's empty
 localStorage.setItem('container', '<div class="add section no-select"><span>Add new </span><svg class="plus-svg" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"></path></svg></div>');
-
-// add button
-$('.add').on('click', function() {
-  $(this).before(domManager.createCont().clone());
-});
 
 
 // all the eventlisteners
@@ -24,4 +20,7 @@ $(document).on('mouseenter', '.del-sect', domManager.showEdit)
   .on('click', '.edit', domManager.changeName)
   .on('input', '.new-name-inp', domManager.dynamicNewNameChange)
   .on('click', '.save', domManager.save)
-  .on('click', '.load', domManager.load);
+  .on('click', '.load', domManager.load)
+  .on('click', 'add', function() {
+    $(this).before(domManager.createCont().clone());
+  });
